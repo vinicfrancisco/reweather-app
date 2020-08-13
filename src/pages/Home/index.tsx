c51ce074162cacc77c2b71561f63737dbbcbf23d
vinicfrancisco <vini.ibi@gmail.com>
2020-08-13 14:15:42 -0300

    created cities list structure

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -10,6 +10,16 @@ import {
   HeaderTitle,
   HeaderWeatherTitle,
   SearchButton,
+  CityContainer,
+  InfoContainer,
+  CityName,
+  TemperatureRange,
+  WeatherDescription,
+  Temperature,
+  WeatherContainer,
+  TempeatureContainer,
+  FavoriteButton,
+  CitiesList,
 } from './styles';
 
 const Home: React.FC = () => {
@@ -31,6 +41,31 @@ const Home: React.FC = () => {
           <Icon name="search" size={28} color={colors.orange} />
         </SearchButton>
       </Header>
+
+      <CitiesList
+        data={[1, 2, 3, 4, 5]}
+        keyExtractor={(city) => city}
+        renderItem={({ item: city }) => (
+          <CityContainer>
+            <InfoContainer>
+              <CityName>Blumenau</CityName>
+              <WeatherDescription>Nublado</WeatherDescription>
+              <TemperatureRange>14ºC - 23ºC</TemperatureRange>
+            </InfoContainer>
+
+            <WeatherContainer>
+              <TempeatureContainer>
+                <Temperature>23ºC</Temperature>
+                <Icon name="cloud" size={40} color={colors.gray} />
+              </TempeatureContainer>
+
+              <FavoriteButton onPress={() => {}}>
+                <Icon name="heart" size={26} color="#f00" />
+              </FavoriteButton>
+            </WeatherContainer>
+          </CityContainer>
+        )}
+      />
     </Container>
   );
 };
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components/native';
+import { FlatList } from 'react-native';
 import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 import colors from '~/styles/colors';
 
@@ -28,3 +29,57 @@ export const HeaderWeatherTitle = styled.Text`
 `;
 
 export const SearchButton = styled.TouchableOpacity``;
+
+export const CitiesList = styled(FlatList as new () => FlatList)`
+  padding: 32px 24px 16px;
+`;
+
+export const CityContainer = styled.View`
+  background: ${colors.black};
+  border-radius: 10px;
+  flex-direction: row;
+  justify-content: space-between;
+  margin-bottom: 16px;
+  padding: 20px;
+`;
+
+export const InfoContainer = styled.View``;
+
+export const CityName = styled.Text`
+  color: ${colors.orange};
+  font-size: 26px;
+  font-family: 'Roboto-Medium';
+  margin-bottom: 8px;
+`;
+
+export const WeatherDescription = styled.Text`
+  color: ${colors.white};
+  font-size: 20px;
+  font-family: 'Roboto-Regular';
+  margin-bottom: 4px;
+`;
+
+export const TemperatureRange = styled.Text`
+  color: ${colors.gray};
+  font-size: 18px;
+`;
+
+export const WeatherContainer = styled.View`
+  align-items: flex-end;
+  justify-content: space-between;
+  flex: 1;
+`;
+
+export const TempeatureContainer = styled.View`
+  align-items: center;
+  flex-direction: row;
+`;
+
+export const Temperature = styled.Text`
+  color: ${colors.white};
+  font-family: 'Roboto-Medium';
+  font-size: 30px;
+  margin-right: 16px;
+`;
+
+export const FavoriteButton = styled.TouchableOpacity``;
