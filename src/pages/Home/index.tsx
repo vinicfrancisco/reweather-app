0472ea1bfc8add0330a745e3b7d20c5a79ede349
vinicfrancisco <vini.ibi@gmail.com>
2020-08-13 09:09:42 -0300

    created home page header layout

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,30 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
+
+import colors from '~/styles/colors';
+
+import {
+  Container,
+  Header,
+  HeaderTitle,
+  HeaderWeatherTitle,
+  SearchButton,
+} from './styles';
 
 const Home: React.FC = () => {
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <Container>
+      <Header>
+        <HeaderTitle>
+          ReWeather {'\n'}
+          <HeaderWeatherTitle>Aqui estão suas cidades</HeaderWeatherTitle>
+        </HeaderTitle>
+
+        <SearchButton onPress={() => {}}>
+          <Icon name="search" size={28} color={colors.orange} />
+        </SearchButton>
+      </Header>
+    </Container>
   );
 };
 
---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,30 @@
+import styled from 'styled-components/native';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+import colors from '~/styles/colors';
+
+export const Container = styled.View`
+  flex: 1;
+`;
+
+export const Header = styled.View`
+  align-items: center;
+  background: ${colors.black};
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 24px;
+  padding-top: ${getStatusBarHeight() + 24}px;
+`;
+
+export const HeaderTitle = styled.Text`
+  color: ${colors.orange};
+  font-family: 'Roboto-Medium';
+  font-size: 20px;
+  line-height: 28px;
+`;
+
+export const HeaderWeatherTitle = styled.Text`
+  color: ${colors.white};
+  font-family: 'Roboto-Regular';
+`;
+
+export const SearchButton = styled.TouchableOpacity``;
