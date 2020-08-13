63807f3da924733ce3e1e834f3484592f98804a3
vinicfrancisco <vini.ibi@gmail.com>
2020-08-13 14:45:17 -0300

    created search page structure

--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -1,13 +1,82 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
+import { useNavigation } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/Feather';
 
-import { Container, Header, HeaderTitle } from './styles';
+import {
+  Container,
+  Header,
+  BackButton,
+  SearchContainer,
+  SearchInput,
+  SearchIcon,
+  CitiesList,
+  CityContainer,
+  CityName,
+  AddCityButton,
+} from './styles';
+import colors from '~/styles/colors';
 
 const Search: React.FC = () => {
+  const { goBack } = useNavigation();
+
+  const [searchValue, setSearchValue] = useState('');
+  const [isFocused, setIsFocused] = useState(false);
+
+  const navigateBack = useCallback(() => {
+    goBack();
+  }, [goBack]);
+
+  const handleChangeInput = useCallback((value: string) => {
+    setSearchValue(value);
+  }, []);
+
+  const handleInputFocus = useCallback(() => {
+    setIsFocused(true);
+  }, []);
+
+  const handleInputBlur = useCallback(() => {
+    setIsFocused(false);
+  }, []);
+
   return (
     <Container>
       <Header>
-        <HeaderTitle>Search</HeaderTitle>
+        <BackButton onPress={navigateBack}>
+          <Icon name="x" size={24} color={colors.gray} />
+        </BackButton>
+
+        <SearchContainer>
+          <SearchIcon
+            name="search"
+            size={20}
+            color={isFocused ? colors.orange : colors.gray}
+          />
+
+          <SearchInput
+            keyboardAppearance="dark"
+            placeholder="Buscar cidade"
+            placeholderTextColor={colors.gray}
+            value={searchValue}
+            onFocus={handleInputFocus}
+            onBlur={handleInputBlur}
+            onChangeText={handleChangeInput}
+          />
+        </SearchContainer>
       </Header>
+
+      <CitiesList
+        data={[1, 2, 3, 4, 5]}
+        keyExtractor={(city) => city}
+        renderItem={({ item: city }) => (
+          <CityContainer>
+            <CityName>Blumenau</CityName>
+
+            <AddCityButton onPress={() => {}}>
+              <Icon name="plus" size={25} color={colors.orange} />
+            </AddCityButton>
+          </CityContainer>
+        )}
+      />
     </Container>
   );
 };
--- src/pages/Search/styles.ts
+++ src/pages/Search/styles.ts
@@ -1,5 +1,7 @@
 import styled from 'styled-components/native';
+import { FlatList } from 'react-native';
 import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+import FeatherIcon from 'react-native-vector-icons/Feather';
 import colors from '~/styles/colors';
 
 export const Container = styled.View`
@@ -11,13 +13,61 @@ export const Header = styled.View`
   background: ${colors.black};
   flex-direction: row;
   justify-content: space-between;
-  padding: 24px;
+  padding: 16px;
   padding-top: ${getStatusBarHeight() + 24}px;
 `;
 
-export const HeaderTitle = styled.Text`
-  color: ${colors.orange};
+export const BackButton = styled.TouchableOpacity``;
+
+export const SearchContainer = styled.View`
+  align-items: center;
+  background: ${colors.softBlack};
+  border-radius: 10px;
+  border-color: ${colors.softBlack};
+  border-width: 2px;
+  flex: 1;
+  flex-direction: row;
+  height: 48px;
+  margin-left: 8px;
+  padding: 0 16px;
+`;
+
+export const SearchInput = styled.TextInput`
+  color: #fff;
+  flex: 1;
+  font-family: 'Roboto-Regular';
+  font-size: 16px;
+`;
+
+export const SearchIcon = styled(FeatherIcon)`
+  margin-right: 16px;
+`;
+
+export const CitiesList = styled(FlatList as new () => FlatList)`
+  padding: 16px;
+`;
+
+export const CityContainer = styled.View`
+  align-items: center;
+  background: ${colors.black};
+  border-radius: 10px;
+  flex-direction: row;
+  justify-content: space-between;
+  margin-bottom: 16px;
+  padding: 16px;
+`;
+
+export const CityName = styled.Text`
+  color: ${colors.white};
   font-family: 'Roboto-Medium';
   font-size: 20px;
-  line-height: 28px;
+`;
+
+export const AddCityButton = styled.TouchableOpacity`
+  align-items: center;
+  background: ${colors.softBlack};
+  border-radius: 20px;
+  height: 40px;
+  justify-content: center;
+  width: 40px;
 `;
