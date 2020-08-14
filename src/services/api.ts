0c055157eccc7cb527d973c3eb24740a3c924858
Vinícius Catafesta Francisco <vinicfrancisco@gmail.com>
2020-08-14 15:34:04 -0300

    added OpenWeather API key on .env

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const api = axios.create({
   baseURL: 'api.openweathermap.org/data/2.5',
   params: {
-    appid: '56cd1e8db1435f9c17de8b0349caaee9',
+    appid: process.env.OPEN_WEATHER_API_KEY,
   },
 });
 
