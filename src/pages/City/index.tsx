666314f7ba1fbf241e25b7e2cfc53663a0bb2553
Vinicius Catafesta <viniciuscatafesta@MacBook-Air-de-Vinicius.local>
2020-08-14 08:42:10 -0300

    created city page structure

--- src/pages/City/index.tsx
+++ src/pages/City/index.tsx
@@ -1,13 +1,65 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import Icon from 'react-native-vector-icons/Feather';
+import { useNavigation } from '@react-navigation/native';
 
-import { Container, Header, HeaderTitle } from './styles';
+import colors from '~/styles/colors';
+
+import {
+  Container,
+  Header,
+  HeaderTitle,
+  BackButton,
+  DaysList,
+  DayContainer,
+  InfoContainer,
+  DayName,
+  WeatherDescription,
+  TemperatureRange,
+  WeatherContainer,
+  TempeatureContainer,
+  Temperature,
+} from './styles';
 
 const City: React.FC = () => {
+  const { goBack } = useNavigation();
+
+  const handleGoBack = useCallback(() => {
+    goBack();
+  }, [goBack]);
+
   return (
     <Container>
       <Header>
-        <HeaderTitle>City</HeaderTitle>
+        <BackButton onPress={handleGoBack}>
+          <Icon name="chevron-left" size={24} color={colors.gray} />
+        </BackButton>
+
+        <HeaderTitle>Blumenau</HeaderTitle>
       </Header>
+
+      <DaysList
+        data={[1, 2, 3, 4, 5]}
+        keyExtractor={(city) => city}
+        renderItem={({ item: city }) => (
+          <DayContainer>
+            <InfoContainer>
+              <DayName>Segunda</DayName>
+
+              <WeatherDescription>Sol aberto</WeatherDescription>
+
+              <TemperatureRange>20ºC - 25ºC</TemperatureRange>
+            </InfoContainer>
+
+            <WeatherContainer>
+              <TempeatureContainer>
+                <Temperature>23ºC</Temperature>
+
+                <Icon name="sun" size={40} color={colors.orange} />
+              </TempeatureContainer>
+            </WeatherContainer>
+          </DayContainer>
+        )}
+      />
     </Container>
   );
 };
