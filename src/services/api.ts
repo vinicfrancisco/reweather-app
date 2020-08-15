1b6afeafccfa0a1759cae1659d2f895bd4008bb6
Vinícius Catafesta Francisco <vinicfrancisco@gmail.com>
2020-08-15 10:44:47 -0300

    fixed axios service

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,12 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'api.openweathermap.org/data/2.5',
-  params: {
-    appid: process.env.OPEN_WEATHER_API_KEY,
-    units: 'metric',
-    lang: 'pt',
-  },
+  baseURL: 'http://api.openweathermap.org/data/2.5',
 });
 
 export default api;
