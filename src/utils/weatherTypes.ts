a013a986289639dbf4a06c19503894c3ec270dcf
Vinícius Catafesta Francisco <vinicfrancisco@gmail.com>
2020-08-16 17:40:38 -0300

    added dinamic color

--- src/utils/weatherTypes.ts
+++ src/utils/weatherTypes.ts
@@ -1,6 +1,7 @@
 interface WeatherType {
   icon: string;
   title: string;
+  color: string;
 }
 
 interface WeatherTypesList {
@@ -11,26 +12,32 @@ const WeatherTypes: WeatherTypesList = {
   ThunderStorm: {
     icon: 'cloud-lightning',
     title: 'Tempestade',
+    color: 'gray',
   },
   Drizzle: {
     icon: 'cloud-drizzle',
     title: 'Chuvisco',
+    color: 'gray',
   },
   Rain: {
     icon: 'cloud-rain',
     title: 'Chuva',
+    color: 'gray',
   },
   Snow: {
     icon: 'cloud-snow',
     title: 'Neve',
+    color: 'gray',
   },
   Clear: {
     icon: 'sun',
     title: 'Tempo Limpo',
+    color: 'orange',
   },
   Clouds: {
     icon: 'cloud',
     title: 'Nublado',
+    color: 'gray',
   },
 };
 
